2f790d9c82af3813a5eaa106b7a3f4baab98fb9d
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-02-12 23:14:05 -0500

    Time: 18 ms (5.18%), Space: 84.1 MB (49.35%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy=10000,sell=0,val=0, max=0;
+        for (int i=0;i<prices.length;i++){
+            if (buy>prices[i]){
+                buy=prices[i];
+                sell=prices[i];
+            }
+            if (sell < prices[i])
+            {   
+                System.out.println(buy+""+sell);
+                sell=prices[i];
+                val=sell-buy;
+                if (max<val){
+                    max=val;
+                }
+            }
+        }
+        //System.out.println(buy+""+sell);
+        return max;
+    }
+}
