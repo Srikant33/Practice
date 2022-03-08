ef7a48c04cd7159d86aab1f967c08ddac34eefbe
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-03-08 15:49:21 -0500

    Time: 17 ms (10.01%), Space: 40.2 MB (51.46%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,11 +2,8 @@ class Solution {
     // int[] 
     public int fib(int n) {
         // int[] a= new int[n+1];
-        if (n==0){
-            return 0;
-        }
-        else if (n==1){
-            return 1;
+        if (n==0 || n==1){
+            return n;
         }
         return fib(n-1)+fib(n-2);
     }
