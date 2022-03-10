926fef0f0176698ab987adf5f25ab59d87f15ecd
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-03-10 03:31:34 -0500

    Time: 0 ms (100.00%), Space: 41 MB (28.15%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp= new int [n];
+        if (n<3){
+            return n;
+        }
+        dp[n-1]=1;
+        dp[n-2]=2;
+        
+        for (int i=n-3;i>=0;i--){
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+        return dp[0];
+        
+    }
+}
