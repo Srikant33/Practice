4369a98dff6bf0d2335d30b416426efbaff16851
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-03-15 18:11:09 -0400

    Time: 108 ms (5.42%), Space: 49 MB (5.09%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,43 +1,21 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        if (nums.length==1){
-            return nums[0];
+        Arrays.sort(nums);
+        int one=0, two=0, temp=0;
+        
+        HashMap<Integer, Integer> hm= new HashMap<>();
+        for (int i=0 ;i <nums.length; i++){
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+nums[i]);
         }
-        HashMap <Integer, Integer> hm= new HashMap<>();
-        for (int n: nums){
-            if (hm.containsKey(n)){
-                hm.put (n, hm.get(n)+n);
-            }
-            else{
-                hm.put(n, n);
-            }
+        System.out.println(hm);
+        for (int i=0;i <=nums[nums.length-1]; i++){
+            temp=two;
+            two=Math.max(one+hm.getOrDefault(i, 0), two);
+            one=temp;
+            System.out.println(i+" "+one+" "+two);
         }
         
-        int[] u= new int[hm.size()];
-        int[] val= new int[hm.size()];
-        int j=0;
-        for (int key: hm.keySet()){
-            u[j]= key;
-            j++;
-        }
-
-        Arrays.sort(u);
-        for (int i=0;i<u.length; i++){
-            if (i>1 && u[i]!=u[i-1]+1)
-                val[i]=Math.max(val[i-1],val[i-2]) +hm.get(u[i]);
-            else if (i>1 && u[i]==u[i-1]+1){
-                val[i]=Math.max(val[i-2]+hm.get(u[i]), val[i-1]);
-            } else if (i==1 && u[i]==u[i-1]+1 ){
-                val[i]= Math.max(val[i-1], hm.get(u[i]));
-            }
-            else if (i==1 && u[i]!=u[i-1]+1 ){
-                val[i]= val[i-1]+hm.get(u[i]);
-            }
-            else
-                val[i]=hm.get(u[i]);
-            
-            System.out.println(val[i]);
-        }
-        return Math.max(val[val.length-1],val[val.length-2]);
+        return Math.max(one, two);
+        
     }
 }
