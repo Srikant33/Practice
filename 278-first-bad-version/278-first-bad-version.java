61dff0c8d7c6e8b876fe7a5fe6e1509b958d46a9
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-04-13 10:30:18 -0400

    Time: 36 ms (6.48%), Space: 39.1 MB (86.36%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -2,30 +2,21 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-    
-    
-    int binarySearch(int l, int r)
-    {
-        if (r >= l) 
-        {
-            int mid = l + (r - l) / 2;
- 
-            if (isBadVersion(mid)==true && isBadVersion(mid-1)==false)
+    public int firstBadVersion(int n) {
+        int low=0, high=n, mid=0;
+        
+        while (low<=high){
+            mid=low+(high-low)/2;
+            if (isBadVersion(mid) && !isBadVersion(mid-1)){
                 return mid;
- 
-            if (isBadVersion(mid)==true)
-                return binarySearch(l, mid - 1);
- 
-            return binarySearch(mid + 1, r);
+            }
+            else if (!isBadVersion(mid)){
+                low=mid+1;
+            }
+           else if (isBadVersion(mid)){
+                high=mid-1;
+            }
         }
         return -1;
-    }
-     
-    
-    public int firstBadVersion(int n) {
-        int a,b;
-        int l=0;
-        int r=n;
-        return binarySearch(l,r);
     }
 }
