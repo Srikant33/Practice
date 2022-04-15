ba40783a2abcf833daffe8eee5f4f15f8afc891b
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-04-14 20:41:11 -0400

    Time: 2 ms (18.45%), Space: 41.8 MB (87.65%) - LeetHub

---
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode ret;
+    
+    public TreeNode BST(TreeNode root, int low, int high){
+        
+        if (root==null){
+            return null;
+        }
+        
+        else if (root.val<low){
+            System.out.println("nul");
+            root=BST(root.right, low, high);
+            if (root==null){
+                return null;
+            }
+        }
+        else if (root.val>high){
+            root=BST(root.left, low, high);
+            if (root==null){
+                return null;
+            }
+            
+        }
+        // System.out.println(root.val);
+        
+        root.left=BST(root.left, low, high);
+        
+        root.right=BST(root.right, low, high);
+        
+        return root;
+    }
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        root=BST(root, low, high);
+        return root;
+    }
+}
