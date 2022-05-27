4141520c54e76e26747f5b03e50d93d5bc8d1bab
Srikant <ksrikantiyer33@gmail.com>
2022-05-27 12:53:07 -0400

    Time: 1 ms (31.55%), Space: 47.8 MB (10.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,38 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int low =0, high = nums.length-1, mid;
+        int low=0, high= nums.length-1, mid;
         int[] ret= new int[]{-1,-1};
-        while (low<=high ){
-            mid=low+(high-low)/2;
-            if (nums[mid]==target && (mid==0 || nums[mid-1]!=target)){
+        // if (high==0){
+        //     return ret;
+        // }
+        // System.out.println(low+""+high);
+        while (low<=high){
+            mid= low+(high-low)/2;
+            // System.out.println(nums[mid]);
+            if (nums[mid]==target && (mid==0 || nums[mid-1]!=target) ){
+                // System.out.println("aa");
                 ret[0]=mid;
-                 // System.out.println(" ");
-                int low1 =mid, high1= nums.length-1, mid1 ;
-                while (low1<=high1){
-                    mid1=low1+(high1-low1)/2;
-                    if (nums[mid1]==target && (mid1==nums.length-1 || nums[mid1+1]>target ) ){
-                        ret[1]=mid1;
-                        break;
+                int l=mid, h=nums.length-1, m;
+                while (l<=h){
+                    m= l+(h-l)/2;
+                    if (nums[m]==target && ( m==nums.length-1 || nums[m+1]!=target)){
+                        ret[1]=m;
                     }
-                    else if (nums[mid1]==target){
-                        low1=mid1+1;
+                    if (target<nums[m]){
+                        h= m-1;
                     }
-                    else if (nums[mid1]>target){
-                        high1=mid1-1;
+                    else{
+                        l=m+1;
                     }
-                    // System.out.println(low1+" "+mid1+" "+high1);
                 }
                 break;
             }
-            else if (nums[mid]<target){
-                low=mid+1;
+            else if (target>nums[mid]){
+                low= mid+1;
             }
-            else 
+            else{
                 high=mid-1;
-            // System.out.println(low+" "+mid+" "+high);
+            }
         }
         return ret;
     }
