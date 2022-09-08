7bc10e1f97ad99f25800879740fff932d510fb59
Srikant <ksrikantiyer33@gmail.com>
2022-09-08 18:40:14 -0500

    Time: 23 ms (7.30%), Space: 45.9 MB (17.61%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int min=Integer.MAX_VALUE,count =0;
-        // List<int[]> l = new LinkedList<>();
         PriorityQueue<Integer> pq= new PriorityQueue<>((a,b)-> a-b);
         
         for (int[] i: intervals){
