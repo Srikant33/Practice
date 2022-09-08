3c15c875467a7c9486d53b16f3fd1879a8558dca
Srikant <ksrikantiyer33@gmail.com>
2022-09-07 21:53:56 -0500

    Time: 505 ms (5.03%), Space: 83.1 MB (43.08%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -4,29 +4,22 @@ class BrowserHistory {
     public BrowserHistory(String homepage) {
         l= new LinkedList<String>();
         l.add(homepage);
-        // System.out.println(l);
         p++;
-        // System.out.print(p);
     }
     
     public void visit(String url) {
         l=l.subList(0,p+1);
-        // System.out.println(l);
         l.add(url);
         p++;
-        // System.out.print(p);
     }
     
     public String back(int steps) {
         p=Math.max(p-steps, 0);
-        // System.out.print(" b "+p);
-        // System.out.println(l);
         return l.get(p);
     }
     
     public String forward(int steps) {
         p=Math.min(p+steps, l.size()-1);
-        // System.out.print(" f "+p);
         return l.get(p);
         
     }
