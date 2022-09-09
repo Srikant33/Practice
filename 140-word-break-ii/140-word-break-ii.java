55605b73a3480a65195c6d6b1cf856e03c62cc99
Srikant <ksrikantiyer33@gmail.com>
2022-09-08 23:26:17 -0500

    Time: 7 ms (45.00%), Space: 43.3 MB (5.69%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -1,18 +1,12 @@
 class Solution {
-    public List<String> helper (String s, List<String> wordDict){
-        
-        List<String> l = new LinkedList<>();
-       
+    public List<String> wordBreak(String s, List<String> wordDict) {
+       List<String> l = new LinkedList<>();
         List<String> ret = new LinkedList<>();
-        
         for (int i =1; i<=s.length(); i++){
             if (wordDict.contains(s.substring(0,i))){
                 if (i<s.length()){
-                    l = helper(s.substring(i, s.length()), wordDict);
-                    // System.out.println(l);
-                    
-                    for (String str: l){
-                        
+                    l = wordBreak(s.substring(i, s.length()), wordDict);       
+                    for (String str: l){         
                         ret.add(s.substring(0,i)+" "+str);
                     }
                 }
@@ -21,25 +15,6 @@ class Solution {
                 }
             }
         }
-        
-
         return ret;
-    }
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> l = helper (s, wordDict);
-        // List<String> ret = new LinkedList<>();
-        // System.out.println(l);
-        // for (String str: l){
-        //     int count = 0;
-        //     for (char c: str.toCharArray()){
-        //         if (c!=' '){
-        //             count++;
-        //         }
-        //     }
-        //     if (count == s.length()){
-        //         ret.add(str);
-        //     }
-        // }
-        return l;
     }
 }
