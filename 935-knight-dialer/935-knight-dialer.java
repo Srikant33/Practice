5435319d917788bc0baafd0f8078a73a656af5e4
Srikant <ksrikantiyer33@gmail.com>
2022-09-10 20:20:32 -0500

    Time: 1383 ms (6.29%), Space: 147 MB (5.05%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.java
+++ 935-knight-dialer/935-knight-dialer.java
@@ -1,29 +1,45 @@
 class Solution {
+    HashMap<Integer, List<Integer>> hm= new HashMap<>();
+    HashMap<Pair<Integer, Integer>, Long> dp= new HashMap<>();
+    
+    public long helper (int n, int prev){
+        if (n==1){
+            return 1;
+        }
+        if (dp.containsKey(new Pair<>(n, prev))){
+            return dp.get(new Pair<>(n, prev));
+        }
+        long sum=0;
+        List<Integer> l = hm.get(prev);
+        for (int i: l){
+            sum= (sum + helper(n-1, i))%1000000007;
+        }
+        dp.put(new Pair<>(n, prev), sum);
+        // System.out.println(sum);
+        return sum;
+    }
+    
     public int knightDialer(int n) {
-        long[] dp = new long[10];
-        long[] temp = new long[10];
-        Arrays.fill(dp,1);
-        for (int i =0 ;i<n; i++){
-            temp[1] = dp[8]+dp[6] ;
-            temp[2] = dp[7]+dp[9] ;
-            temp[3] = dp[4]+dp[8] ;
-            temp[4] = dp[3]+dp[9]+dp[0] ;
-            temp[5] = 0 ;
-            temp[6] = dp[1]+dp[7]+dp[0] ;
-            temp[7] = dp[2]+dp[6] ;
-            temp[8] = dp[1]+dp[3] ;
-            temp[9] = dp[2]+dp[4] ;
-            temp[0] = dp[4]+dp[6] ;
-            
-            for (int j=0; j<10; j++) temp[j] = temp[j]% 1000000007;
-            long[] arr = dp;
-            dp =temp; 
-            temp = arr;
+        hm.put(1, new ArrayList<Integer>(Arrays.asList(6,8)));
+        hm.put(2, new ArrayList<Integer>(Arrays.asList(7,9)));
+        hm.put(3, new ArrayList<Integer>(Arrays.asList(4,8)));
+        hm.put(4, new ArrayList<Integer>(Arrays.asList(3,9,0)));
+        hm.put(5, new ArrayList<Integer>(Arrays.asList()));
+        hm.put(6, new ArrayList<Integer>(Arrays.asList(1,7,0)));
+        hm.put(7, new ArrayList<Integer>(Arrays.asList(2,6)));
+        hm.put(8, new ArrayList<Integer>(Arrays.asList(1,3)));
+        hm.put(9, new ArrayList<Integer>(Arrays.asList(4,2)));
+        hm.put(0, new ArrayList<Integer>(Arrays.asList(4,6)));
+
+        long total=0;
+        for (int i=0; i<10; i++){
+            total = (total + helper(n, i))%1000000007;
+            // System.out.println(total);
+            // total = (long)(total%(Math.pow(10,9)+7));
         }
         
-        long res =0;
-        for (int i=0 ;i<10; i++) res = (res + temp[i])%1000000007;
+        return (int)total;
         
-        return (int)res;
+        
     }
 }
