a71b940e0e7867e8e3aafc149b2ddca2d803bd55
Srikant <ksrikantiyer33@gmail.com>
2022-10-09 20:52:07 -0500

    Time: 11 ms (21.04%), Space: 72.2 MB (80.34%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.java
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.java
@@ -2,16 +2,11 @@ class Solution {
     public long minimumHealth(int[] damage, int armor) {
         
         long max =0, sum =0; 
-        //traversre to find mnax 
         
         for (int i: damage){
             max= Math.max(i, max);
             sum+= i;
         }
-        
-        // System.out.println(sum);
-        
-        //find amt required 
         return sum +1 - ((max>armor)?armor : max); 
     }
 }
