757acb4f89708f325f2dd452ad6f406c2e53841c
Srikant <ksrikantiyer33@gmail.com>
2022-11-06 10:13:27 -0600

    Time: 27 ms (9.89%), Space: 51.7 MB (6.72%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -10,8 +10,8 @@ class Solution {
             if (num==0){
                 max=1;
                 min=1;
-                ret=Math.max(ret,0);
-                continue;
+                // ret=Math.max(ret,0);
+                // continue;
             }
             int first = max* num;
             int second = min*num;
@@ -20,12 +20,8 @@ class Solution {
             min = Collections.min(Arrays.asList(first, second, num));
             ret= Math.max(ret, max);
 
-           // System.out.println(max+ " "+ min);
 
         }
-
-        
-                //ret= Math.max(ret, max);
         return ret;
 
     }
