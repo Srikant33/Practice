b08e1a76c2c9d7365461235bba5e3f4a6f8efa85
Srikant <ksrikantiyer33@gmail.com>
2023-07-26 11:58:12 -0500

    Time: 17 ms (94.31%) | Memory: 56.8 MB (52.04%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -4,7 +4,6 @@ class Solution {
 
         for (int i=0;i< nums.length; i++){
             if (hm.containsKey(nums[i]) && (i-hm.get(nums[i])) <=k){
-                // System.out.println(nums[i]+" "+i);
                 return true;
             }
             hm.put(nums[i], i);
