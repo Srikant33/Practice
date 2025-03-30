7230d0a03acd19e6f49677942573ca860489a772
Srikant <ksrikantiyer33@gmail.com>
2025-03-29 21:23:43 -0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (57.22%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,19 +1,23 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        //String builder to merge strings 
         StringBuilder sb = new StringBuilder();
-        int pos1=0;
-        // int pos2=0;
 
-        while (pos1<word1.length() && pos1<word2.length()){
-            sb.append(word1.charAt(pos1));
-            sb.append(word2.charAt(pos1));
+        //creeating a int that is the length of the shorter word 
+        int shor = Math.min(word1.length(), word2.length());
 
-            pos1++;
+        //parsing through the words till the shortest word
+        for (int i=0; i<shor; i++){
+            //adding from word 1 then 2
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
         }
 
-        sb.append(word1.substring(pos1, word1.length()));
-        sb.append(word2.substring(pos1, word2.length()));
+        //adding all remaining chars from the longer word ...whciever is shorter will not contribute anything  
+        sb.append(word1.substring(shor, word1.length()));
+        sb.append(word2.substring(shor, word2.length()));
 
+        //returning string
         return sb.toString();
     }
 }
